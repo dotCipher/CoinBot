057c3ae8b7adf93dfb07f9eeb48a6f8f9b23d09e
root <root@localhost.localdomain>
2013-11-08 04:58:33 -0800

    Paving the way for Python port

---
+++ CoinBot.py
@@ -0,0 +1,8 @@
+#####################################################################################
+# File: CoinBot.py
+# Purpose: An 'Ub3r31337' way to automatically visit certain free e-coin sites 
+#          and enter valid address information for payouts.
+# Constraints: Must be run using Python
+# Syntax: python ./BitcoinerBot.py
+#####################################################################################
+
