dd9a40be71750ecd0acb7d8e54664cc7f716fb4a
dot_Cipher <dot_Cipher@null-sec.net>
2013-12-04 03:35:55 -0800

    Adding plugin webscraper

--- CoinBot.py
+++ CoinBot.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 #####################################################################################
 # File: CoinBot.py
-# Purpose: An 'Ub3r31337' way to automatically visit certain free e-coin sites 
-#          and enter valid address information for payouts.
+# Purpose: An 'Ub3r31337' way to auto manage digital currencies
 # Constraints: Must be run using Python
 # Syntax: ./CoinBot.py
 ####################################################################################
@@ -26,10 +25,15 @@ _MODULES_FOLDER_NAME="Modules"
 _MODULES_DIR=os.path.join(_MAIN_DIR,_MODULES_FOLDER_NAME)
 _SETUP_FOLDER_NAME="Setup"
 _SETUP_DIR=os.path.join(_MAIN_DIR,_SETUP_FOLDER_NAME)
+_PLUGINS_FOLDER_NAME="Plugins"
+_PLUGINS_DIR=os.path.join(_MAIN_DIR, _PLUGINS_FOLDER_NAME)
 _LIB_FOLDER_NAME="Libs"
 _LIB_DIR=os.path.join(_MAIN_DIR,_LIB_FOLDER_NAME)
 _LOGS_FOLDER_NAME="Logs"
 _LOGS_DIR=os.path.join(_MAIN_DIR,_LOGS_FOLDER_NAME)
+# Register Plugins here
+_SCRAPER_FOLDER_NAME="Scraper"
+_SCRAPER_DIR=os.path.join(_PLUGINS_DIR, _SCRAPER_FOLDER_NAME)
 # File Location variables
 _CORE_LOG_NAME="CB_Core.log"
 _CORE_LOG_FILE=os.path.join(_LOGS_DIR,_CORE_LOG_NAME)
@@ -49,47 +53,55 @@ if modules_subfolder not in sys.path:
 libs_subfolder = os.path.realpath(os.path.abspath(os.path.join(_SCRIPT_FILE,_LIB_FOLDER_NAME)))
 if libs_subfolder not in sys.path:
   sys.path.insert(0, libs_subfolder)
-####################################################################################
-# Command Line Params
+plugins_subfolder = os.path.realpath(os.path.abspath(os.path.join(_SCRIPT_FILE,_PLUGINS_FOLDER_NAME)))
+if plugins_subfolder not in sys.path:
+  sys.path.insert(0, plugins_subfolder)
 #####################################################################################
-# Initializes all accepted CLI args
-# Returns: (void)
-def initCliParams():
-  parser = OptionParser(usage="usage: %prog [options]",
-    version="%prog 1.0")
-  parser.add_option("-e", "--execute-modules",
-    action="store_true", dest="execute_modules",
-    default=False, help="Execute modules",)
-  (options, args) = parser.parse_args()
-#####################################################################################
-# Import all project libraries/Modules
+# Import all project Libraries/Modules/Plugins
 #####################################################################################
 import Libs.CoinBot_Config as cbConfig
 import Libs.CoinBot_Logging as cbLogger
+import Plugins.Scraper.CoinBot_Scraper as cbScraper
 ####################################################################################
-# Module functions
+# Core Module functions
 #####################################################################################
 # Gets a List of CoinBot Modules
 # Returns: List<Module> OR None (Object)
-def runModules():
+# TODO: Move logic to new function for getting list
+def execute_modules():
   if not os.path.exists(_MODULES_DIR):
     os.makedirs(os.path.dirname(os.path.realpath(_MODULES_DIR)))
     return None
   os.chdir(_MODULES_DIR)
   for files in os.listdir("."):
     if (files.endswith(".py")) and (not(files == "__init__.py")):
+      print files
+      print "iteration"
       # TODO: Figure out syntax for executing strings as files in python
-      eval(files)
+      #eval(files)
 
 #####################################################################################
 # Main function
 #####################################################################################
 def main():
-  # Setup command line params
-  initCliParams()
+  # Parse CLI
+  parser = OptionParser(usage="usage: %prog [options]",
+    version="%prog 1.0")
+  parser.add_option("-e", "--execute-modules",
+    action="store_true", dest="execute_modules",
+    default=False, help="Execute modules")
+  parser.add_option("-f", "--fetch-data",
+    action="store_true", dest="fetch_data",
+    default=False, help="Fetch market data")
+  (options, args) = parser.parse_args()
 
   # Run all modules
-  runModules()
+  if(options.execute_modules is True):
+    execute_modules()
+
+  # Fetch all market data
+  if(options.fetch_data is True):
+    cbScraper.fetch_data()
 
   # Initialize Logging
   # TODO: Migrate to module specific call
---
+++ Plugins/Scraper/CoinBot_Scraper.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#####################################################################################
+# File: CoinBot_Scraper.py
+# Purpose: A Scraper module for fetching market data online for CoinBot
+# Constraints: Must be imported into a main namespace Python file
+# Usage: import CoinBot_Scraper
+#####################################################################################
+# Imports
+#####################################################################################
+import os
+import logging
+
+def fetch_data():
+	print "Fetch data here"
